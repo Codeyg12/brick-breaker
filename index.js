e779a158f8dfe0db8b3831d12733108fa95875c4
Codeyg12 <codey.gallup@gmail.com>
2023-02-14 20:23:21 -0700

    function to add bricks and brick styling

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ class Brick {
         this.topRight = [x + blockWidth, y + blockHeight]
     }
 }
+
+let bricks = [
+    new Brick(10, 270)
+]
+
+function addBricks() {
+    for (let i = 0; i < bricks.length; i++) {
+        const brick = document.createElement('div')
+        brick.classList.add('brick')
+        brick.style.left = bricks[i].bottomLeft[0] + 'px'
+        brick.style.bottom = bricks[i].bottomLeft[1] + 'px'
+        playArea.appendChild(brick)
+    }
+}
+
+addBricks()
