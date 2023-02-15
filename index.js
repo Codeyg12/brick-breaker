bb33afe51caa84e89c247cb56eb6ba6fb66529a9
Codeyg12 <codey.gallup@gmail.com>
2023-02-14 20:42:53 -0700

    starting move player function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const playArea = document.querySelector('.play-area')
 const scoreBoard = document.getElementById('score')
 const blockWidth = 100
 const blockHeight = 20
+const playerStart = [230, 10]
+let currentPosition = playerStart
 
 class Brick {
     constructor(x, y) {
@@ -34,10 +36,28 @@ function addBricks() {
     for (let i = 0; i < bricks.length; i++) {
         const brick = document.createElement('div')
         brick.classList.add('brick')
-        brick.style.left = bricks[i].bottomLeft[0] + 'px'
-        brick.style.bottom = bricks[i].bottomLeft[1] + 'px'
+        brick.style.left = `${bricks[i].bottomLeft[0]}px`
+        brick.style.bottom = `${bricks[i].bottomLeft[1]}px`
         playArea.appendChild(brick)
     }
 }
 
 addBricks()
+
+const player = document.createElement('div')
+player.classList.add('player')
+player.style.left = `${currentPosition[0]}px`
+player.style.bottom = `${currentPosition[1]}px`
+playArea.appendChild(player)
+
+function movePlayer(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            player.style.left = `${currentPosition[0]++}px`
+            break;
+        case 'ArrowRight':
+            break;
+        default:
+            break;
+    }
+}
